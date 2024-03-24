a371b5f0ab6f8067f64bc62373698469ef06721c
Sajal Sisodia <sajal89304@gmail.com>
2024-03-24 19:03:17 +0530

    first commit

---
+++ find.py
@@ -0,0 +1,9 @@
+
+
+#fine the fectorial
+
+import fectorial
+from fectorial.fect import resfindfect 
+
+n = int(input("Enter the numer to  find factorial : "))
+print("The given number factorial is ",resfindfect(n))
